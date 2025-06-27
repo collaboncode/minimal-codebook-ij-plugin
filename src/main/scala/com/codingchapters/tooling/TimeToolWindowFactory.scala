560c41e67cf3795fdca2932b9446d88ac7dc5b13
techlead <techlead@codogenics.com>
2025-06-28 02:28:28 +0530

    little refactoring

--- src/main/scala/com/codingchapters/tooling/TimeToolWindowFactory.scala
+++ src/main/scala/com/codingchapters/tooling/TimeToolWindowFactory.scala
@@ -1,21 +1,35 @@
 package com.codingchapters.tooling
 
+import com.intellij.icons.AllIcons
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.{ToolWindow, ToolWindowFactory}
 import com.intellij.ui.content.ContentFactory
 import com.intellij.ui.jcef.JBCefBrowser
 import com.intellij.openapi.util.Key
-import com.intellij.openapi.actionSystem.{ActionManager, ActionPlaces}
+import com.intellij.openapi.actionSystem.{ActionManager, ActionPlaces, AnAction, DefaultActionGroup}
+
 import javax.swing.JPanel
 import java.awt.BorderLayout
 
 class TimeToolWindowFactory extends ToolWindowFactory with TimeHtmlGenerator {
-  
+
+  implicit class ToolbarActionExtensions(baseAction: BaseAction) {
+    def withIcon(tooltip: String, icon: javax.swing.Icon): AnAction = {
+      val newAction: DummyAction = new DummyAction()
+      newAction.getTemplatePresentation.setIcon(icon)
+      newAction.getTemplatePresentation.setText(tooltip)
+      newAction
+    }
+  }
+
   override def createToolWindowContent(project: Project, toolWindow: ToolWindow): Unit = {
     val browser = new JBCefBrowser()
-    
-    // Create the toolbar
-    val toolBarActionGroup = new com.codingchapters.tooling.DummyActionGroup()
+
+    val toolBarActionGroup = new DefaultActionGroup()
+
+    val timeAction = new DummyAction().withIcon("Show Current Time", AllIcons.General.Information)
+    toolBarActionGroup.addAction(timeAction)
+
     val toolbar = ActionManager.getInstance().createActionToolbar(ActionPlaces.TOOLWINDOW_TITLE, toolBarActionGroup, true)
     
     // Create main panel and add toolbar and browser
